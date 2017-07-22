18e5a6b1896a4a3d430a95c7e5e24d230609d2c5
Chris Szymansky <chris.szymansky@gmail.com>
2017-07-22 14:39:35 -0700

    Adds Link from react-router-dom

--- 03 04/src/components/Main.js
+++ 03 04/src/components/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Main extends React.Component {
   render() {
     return (
 		<div>
 			<h1>
-				Reduxstagram
+				<Link to="/">Reduxstagram</Link>
 			</h1>
 		</div>
     )
