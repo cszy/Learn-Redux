de19fd4db27492b8df899a668fb0c785e8aa72a2
Chris Szymansky <chris.szymansky@gmail.com>
2017-07-22 18:16:37 -0700

    Adds Provider to Router

--- 05 06 07 08/src/index.js
+++ 05 06 07 08/src/index.js
@@ -10,15 +10,17 @@ import PhotoGrid from './components/PhotoGrid';
 import Single from './components/Single';
 
 const router = (
-	<BrowserRouter>
-		<div>
-			<Route path='/' component={Main} />
-			<Switch>
-			    <Route exact path='/' component={PhotoGrid} />
-			    <Route path="/view/:postId" component={Single}></Route>
-			</Switch>
-		</div>
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter history={history}>
+			<div>
+				<Route path='/' component={Main} />
+				<Switch>
+			    	<Route exact path='/' component={PhotoGrid} />
+			    	<Route path="/view/:postId" component={Single}></Route>
+				</Switch>
+			</div>
+		</BrowserRouter>
+	</Provider>
 )
 
 ReactDOM.render(router, document.getElementById('root'));
